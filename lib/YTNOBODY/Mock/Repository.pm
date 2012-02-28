9454acd9e1ceb331d6154ff24cf793e0d985e15c
ytnobody / satoshi azuma <ytnobody@gmail.com>
2012-02-28 19:02:31 +0900

    ini

---
+++ lib/YTNOBODY/Mock/Repository.pm
@@ -0,0 +1,32 @@
+package YTNOBODY::Mock::Repository;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+YTNOBODY::Mock::Repository -
+
+=head1 SYNOPSIS
+
+  use YTNOBODY::Mock::Repository;
+
+=head1 DESCRIPTION
+
+YTNOBODY::Mock::Repository is
+
+=head1 AUTHOR
+
+satoshi azuma E<lt>ytnobody at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
